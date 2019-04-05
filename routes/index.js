c1ac7745f41bbd29ed087ade7264bf3437e9980d
root <root@ip-172-31-89-222.ec2.internal>
2019-04-05 18:06:18 +0000

    This is definitely going to work

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Most Splediferous Achievement!' });
+  res.render('index', { title: 'This is DEFINITELY going to work....THE FIRST TIME!!!' });
 });
 
 module.exports = router;
