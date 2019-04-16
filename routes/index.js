88ab47e4206f6ec3892b5cbdb967f99912e5b1c7
Jared Messer <JMESSER@www.nslc.org>
2019-04-16 10:02:01 -0400

    Let's go!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is DEFINITELY going to work....THE FIRST TIME!!!' });
+  res.render('index', { title: 'ZERO DEATHS!!!' });
 });
 
 module.exports = router;
