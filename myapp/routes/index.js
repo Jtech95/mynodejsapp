f8c8019afdf84c3c4aa24bbf876d9c029c562f58
Jared Messer <JMESSER@www.nslc.org>
2019-04-16 09:45:47 -0400

    Updated index page

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'the new and improved meme review. zero deaths, guys.' });
 });
 
 module.exports = router;
