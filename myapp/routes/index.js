296505a469ef9d4455b387c7ba6735104183ded3
Jared Messer <JMESSER@www.nslc.org>
2019-04-16 09:50:45 -0400

    One more

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'the new and improved meme review. zero deaths, guys.' });
+  res.render('index', { title: 'the new and improved meme review. zero deaths, guys. we are still relavent.' });
 });
 
 module.exports = router;
